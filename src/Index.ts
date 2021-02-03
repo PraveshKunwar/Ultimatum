fd0848b7933f80ea055d524a511642d021f2cdc0
PraveshKunwar <praveshisawesome2017@gmail.com>
2021-02-03 17:11:25 -0500

    Starting over!

--- src/Index.ts
+++
@@ -1,5 +0,0 @@
-import { Token } from './interfaces/Token';
-import * as File from '../token.json';
-import Bot from './config/Bot';
-const BotConfig: Token = File;
-new Bot().start(BotConfig);
--- src/events/Client/ReadyEvent.ts
+++
@@ -1,7 +0,0 @@
-import { Run } from '../../interfaces/Evts';
-
-export const run: Run = async (client) => {
-	client.Logger.success(`${client.user.tag} is online baby.`);
-};
-
-export const Name: string = 'ready';
