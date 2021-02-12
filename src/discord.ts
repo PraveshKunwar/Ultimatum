65abeb8847e9596b4c827f615731415855d64b6e
Pravesh Kunwar <praveshk04@outlook.com>
2021-02-11 19:02:15 -0500

    Added LICENSE for copyright reasons.
    Separated commands into separate distinct folders. Added license for security reasons. Will add more commands soon, and mongo db has been setup!

--- src/discord.ts
+++ src/discord.ts
@@ -1,53 +1,102 @@
-import { Client, Collection, Message } from "discord.js";
-import { readdir } from "fs";
+import { Client, Collection, Message } from 'discord.js';
+import { readdir } from 'fs';
 
-import { Command } from "./interfaces/Command";
-import { Event } from "./interfaces/Event";
+import { Command } from './interfaces/Command';
+import { Event } from './interfaces/Event';
 
 const Ultimatum: Client = new Client({
-  disableMentions: "all",
-  fetchAllMembers: true,
+	disableMentions: 'all',
+	fetchAllMembers: true,
 });
 
 export const CmdCollection: Collection<
-  string | string[],
-  Command
+	string | string[],
+	Command
 > = new Collection();
 export const EvtCollection: Collection<
-  string | string[],
-  Event
+	string | string[],
+	Event
 > = new Collection();
 
 export const StartClient = (config: string | any): void => {
-  //cmd handler!
-  readdir("./dist/handlers/commands", (err: any, files: string[] | any[]) => {
-    err ? console.log(err) : false;
-    if (files.length < 0) {
-      return console.log("No commands!");
-    } else {
-      files.forEach((f) => {
-        const CmdName: string = f.split(".")[0];
-        const props = require(`./handlers/commands/${f}`);
-        CmdCollection.set(CmdName, props);
-      });
-    }
-  });
+	//cmd handler!
+	readdir(
+		'./dist/handlers/commands/misc',
+		(err: any, files: string[] | any[]) => {
+			err ? console.log(err) : false;
+			if (files.length < 0) {
+				return console.log('No commands!');
+			} else {
+				files.forEach((f) => {
+					const CmdName: string = f.split('.')[0];
+					const props = require(`./handlers/commands/misc/${f}`);
+					CmdCollection.set(CmdName, props);
+				});
+			}
+		}
+	);
 
-  //evt handler
+	readdir(
+		'./dist/handlers/commands/moderation',
+		(err: any, files: string[] | any[]) => {
+			err ? console.log(err) : false;
+			if (files.length < 0) {
+				return console.log('No commands!');
+			} else {
+				files.forEach((f) => {
+					const CmdName: string = f.split('.')[0];
+					const props = require(`./handlers/commands/moderation/${f}`);
+					CmdCollection.set(CmdName, props);
+				});
+			}
+		}
+	);
+	readdir(
+		'./dist/handlers/commands/music',
+		(err: any, files: string[] | any[]) => {
+			err ? console.log(err) : false;
+			if (files.length < 0) {
+				return console.log('No commands!');
+			} else {
+				files.forEach((f) => {
+					const CmdName: string = f.split('.')[0];
+					const props = require(`./handlers/commands/music/${f}`);
+					CmdCollection.set(CmdName, props);
+				});
+			}
+		}
+	);
 
-  readdir("./dist/handlers/events", (err: any, files: string[] | any[]) => {
-    err ? console.log(err) : false;
+	readdir(
+		'./dist/handlers/commands/profile',
+		(err: any, files: string[] | any[]) => {
+			err ? console.log(err) : false;
+			if (files.length < 0) {
+				return console.log('No commands!');
+			} else {
+				files.forEach((f) => {
+					const CmdName: string = f.split('.')[0];
+					const props = require(`./handlers/commands/profile/${f}`);
+					CmdCollection.set(CmdName, props);
+				});
+			}
+		}
+	);
+	//evt handler
 
-    files.forEach((f: string) => {
-      const EvtName: string = f.split(".")[0];
-      const props = require(`./handlers/events/${f}`);
-      EvtCollection.set(EvtName, props);
-      Ultimatum.on(EvtName, (...args) => {
-        props.run(Ultimatum, ...args);
-      });
-    });
-  });
-  Ultimatum.login(config);
+	readdir('./dist/handlers/events', (err: any, files: string[] | any[]) => {
+		err ? console.log(err) : false;
+
+		files.forEach((f: string) => {
+			const EvtName: string = f.split('.')[0];
+			const props = require(`./handlers/events/${f}`);
+			EvtCollection.set(EvtName, props);
+			Ultimatum.on(EvtName, (...args) => {
+				props.run(Ultimatum, ...args);
+			});
+		});
+	});
+	Ultimatum.login(config);
 };
 
 export default Ultimatum;
