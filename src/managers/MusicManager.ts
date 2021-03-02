9578775c33b9c8719e42ed113dc567b72cfc425a
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-01 23:34:38 -0500

    music bot in works

--- src/managers/MusicManager.ts
+++ src/managers/MusicManager.ts
@@ -1,21 +1,44 @@
 import { Ultimatum } from '../discord';
 import ytdl from 'ytdl-core';
-import { Message } from 'discord.js';
+import { Guild, Message, VoiceChannel, VoiceConnection } from 'discord.js';
 import { MusicTypes } from '../interfaces/MusicTypes';
-import { fileURLToPath } from 'url';
 const yts = require('yt-search');
 
 class MusicManager {
 	public queue = new Map();
-	public async play(vid: string): Promise<void> {
+	public message: Message;
+	public client: Ultimatum;
+	public vc: VoiceChannel;
+	public constructor() {
+		const QueueObject = {
+			currentChannel: this.message.channel,
+			currentVc: this.vc,
+			connection: false,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+		this.queue.set(this.message.guild.id, QueueObject);
+	}
+	public async play(vid: string, guild?: Guild): Promise<void> {
 		const search: MusicTypes = await yts(vid);
 		//first data provided by user :)
-		const title = search.all[0].title;
-		const url = search.all[0].url;
-		const desc = search.all[0].desc;
-		const img = search.all[0].image;
-		const timestamp = search.all[0].timestamp;
-		const views = search.all[0].views;
+		const queue: any = this.queue.get(guild.id);
+		const songs: any[] | unknown[] = queue.songs;
+		const SearchResults = {
+			title: search.all[0].title,
+			url: search.all[0].url,
+			desc: search.all[0].desc,
+			img: search.all[0].image,
+			timestamp: search.all[0].timestamp,
+			views: search.all[0].views,
+		};
+		songs.push(SearchResults);
+		this.vc = this.message.member.voice.channel;
+		if (!this.vc) {
+			//check later;
+		}
+		const connection: VoiceConnection = await this.vc.join();
 	}
 }
 
