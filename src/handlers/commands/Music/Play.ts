c099a782efdea147a578e6309887001936286a11
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-04 23:06:05 -0500

    working on play

--- src/handlers/commands/Music/Play.ts
+++ src/handlers/commands/Music/Play.ts
@@ -1,13 +1,35 @@
 import { Run } from '../../../interfaces/Command';
 import BlockQuote from '../../../util/BlockQuote';
-import mongoose, { AggregationCursor } from 'mongoose';
-import { MessageEmbed } from 'discord.js';
+import mongoose from 'mongoose';
+import { MessageAttachment, MessageEmbed } from 'discord.js';
 import Colors from '../../../util/Colors';
 import ytdl from 'ytdl-core';
 
 export const run: Run = async (client, message, args, prefix) => {
 	console.log(args);
-	//check perms yah yah
+	const vc = message.member.voice.channel;
+	const song: string = args.join(' ');
+	const perms: boolean =
+		!message.guild.me.hasPermission('CONNECT') ||
+		!message.guild.me.hasPermission('SPEAK');
+	if (!vc || perms || song) {
+		const Error = client.ErrorEmbed(
+			`
+		Please make sure you meet the following requirements to use this command:
+		${BlockQuote(`
+		1. Must have permission CONNECT.
+		2. Must have permission SPEAK.
+		3. You must be in a voice channel for me to join.
+		4. Must use the command correctly: ${prefix}play title of song
+		`)}
+		`,
+			client,
+			message
+		);
+		message.channel.send(Error);
+	} else {
+		client.MusicManager.play(song);
+	}
 };
 
 export const name: string = 'play';
--- src/managers/MusicManager.ts
+++ src/managers/MusicManager.ts
@@ -8,11 +8,9 @@ class MusicManager {
 	public queue = new Map();
 	public message: Message;
 	public client: Ultimatum;
-	public vc: VoiceChannel;
 	public constructor() {
 		const QueueObject = {
 			currentChannel: this.message.channel,
-			currentVc: this.vc,
 			connection: false,
 			songs: [],
 			volume: 5,
@@ -23,8 +21,8 @@ class MusicManager {
 	public async play(vid: string, guild?: Guild): Promise<void> {
 		const search: MusicTypes = await yts(vid);
 		//first data provided by user :)
-		const queue: any = this.queue.get(guild.id);
-		const songs: any[] | unknown[] = queue.songs;
+		//const queue: any = this.queue.get(guild.id);
+		//	const songs: any[] | unknown[] = queue.songs;
 		const SearchResults = {
 			title: search.all[0].title,
 			url: search.all[0].url,
@@ -33,12 +31,8 @@ class MusicManager {
 			timestamp: search.all[0].timestamp,
 			views: search.all[0].views,
 		};
-		songs.push(SearchResults);
-		this.vc = this.message.member.voice.channel;
-		if (!this.vc) {
-			//check later;
-		}
-		const connection: VoiceConnection = await this.vc.join();
+		console.log(SearchResults);
+		//	songs.push(SearchResults);
 	}
 }
 
