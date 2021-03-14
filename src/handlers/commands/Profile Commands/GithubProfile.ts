16da0904202bc5b0563537d9e6d77b5841fb656c
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-14 19:11:56 -0400

    git api giving issues

--- src/handlers/commands/Profile Commands/GithubProfile.ts
+++ src/handlers/commands/Profile Commands/GithubProfile.ts
@@ -1,7 +1,7 @@
 import { Run } from '../../../interfaces/Command';
 import ErrorEmbed from '../../../errors/ErrorEmbed';
 import Colors from '../../../util/Colors';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { MessageEmbed } from 'discord.js';
 import GuildModel from '../../../models/GuildModel';
 
@@ -16,46 +16,64 @@ export const run: Run = async (client, message, args, prefix) => {
 		);
 		message.channel.send(Error);
 	} else {
-		axios.get(`https://api.github.com/users/${username}`).then((res: any) => {
-			const date = new Date(Date.parse(res.data.created_at));
-			const month = date.getMonth() + 1;
-			const day = date.getDate();
-			const year = date.getFullYear();
-			const GithubProfile = new MessageEmbed()
-				.setColor(Colors.github_color_palette)
-				.setTimestamp()
-				.setTitle('🕵️ Github Profile')
-				.setAuthor(client.user?.tag, client.user?.displayAvatarURL())
-				.setThumbnail(res.data.avatar_url)
-				.setFooter(
-					`User: ${message.author?.tag} • Created by: PraveshK`,
-					message.author.displayAvatarURL()
-				)
-				.setDescription(`Github Profile displayed for user **${username}**.`)
-				.addFields(
-					{
-						name: '⭐ Bio',
-						value: res.data.bio === null ? 'No Bio!' : res.data.bio,
-					},
-					{ name: '⭐ Followers', value: res.data.followers, inline: true },
-					{ name: '⭐ Following', value: res.data.following, inline: true },
-					{ name: '⭐ Location', value: res.data.location, inline: true },
-					{ name: '⭐ Repos', value: res.data.public_repos, inline: true },
-					{
-						name: '⭐ Account created at:',
-						value: `${month}/${day}/${year}`,
-						inline: true,
-					},
-					{
-						name: `⭐ Link to Github profile`,
-						value: `[${username}'s Link](${res.data.html_url})`,
-						inline: true,
-					}
-				);
-			message.channel.send(GithubProfile).then(async (msg) => {
-				await msg.react('❤️');
+		axios
+			.get(`https://api.github.com/users/${username}`)
+			.then((res: AxiosResponse) => {
+				if (res.status === 200) {
+					const date = new Date(Date.parse(res.data.created_at));
+					const month = date.getMonth() + 1;
+					const day = date.getDate();
+					const year = date.getFullYear();
+					const GithubProfile = new MessageEmbed()
+						.setColor(Colors.github_color_palette)
+						.setTimestamp()
+						.setTitle('🕵️ Github Profile')
+						.setAuthor(client.user?.tag, client.user?.displayAvatarURL())
+						.setThumbnail(res.data.avatar_url)
+						.setFooter(
+							`User: ${message.author?.tag} • Created by: PraveshK`,
+							message.author.displayAvatarURL()
+						)
+						.setDescription(
+							`Github Profile displayed for user **${username}**.`
+						)
+						.addFields(
+							{
+								name: '⭐ Bio',
+								value: res.data.bio === null ? 'No Bio!' : res.data.bio,
+							},
+							{
+								name: '⭐ Followers',
+								value: res.data.followers,
+								inline: true,
+							},
+							{
+								name: '⭐ Following',
+								value: res.data.following,
+								inline: true,
+							},
+							{ name: '⭐ Location', value: res.data.location, inline: true },
+							{
+								name: '⭐ Repos',
+								value: res.data.public_repos,
+								inline: true,
+							},
+							{
+								name: '⭐ Account created at:',
+								value: `${month}/${day}/${year}`,
+								inline: true,
+							},
+							{
+								name: `⭐ Link to Github profile`,
+								value: `[${username}'s Link](${res.data.html_url})`,
+								inline: true,
+							}
+						);
+					message.channel.send(GithubProfile).then(async (msg) => {
+						await msg.react('❤️');
+					});
+				}
 			});
-		});
 	}
 };
 
