917b9894c1c1a9614736033865f3f95059ba8585
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-29 11:23:43 -0400

    auth working, working on setting cookies

--- @website/pages/api/authorized.ts
+++ @website/pages/api/authorized.ts
@@ -1,21 +1,62 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import axios, { AxiosResponse } from 'axios';
-import btoa from 'btoa';
-import { auths } from '../../utils/util';
+import fetch from 'node-fetch';
+import { auths, DataProps, scopes, ReturnTokens } from '../../utils/util';
 require('dotenv').config();
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-	const code = req.query.code; // code returned from auth.
-	axios
-		.post(`${auths.token}?grant_type=authorization_code&code=${code}`, {
-			headers: {
-				Authorization: `Basic ${btoa(
-					`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-				)}`,
-			},
-		})
-		.then((res: AxiosResponse) => {
-			console.log(res.data);
+const btoa = require('btoa');
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	//@ts-ignore
+	const code: string = req.query.code; // code returned from auth
+	const SCOPES: scopes = [
+		'identify',
+		'email',
+		'connections',
+		'guilds',
+		'guilds.join',
+		'gdm.join',
+		'rpc',
+		'rpc.notifications.read',
+		'rpc.voice.read',
+		'rpc.voice.write',
+		'rpc.activities.write',
+		'messages.read',
+		'relationships.read',
+	];
+	const data: DataProps = {
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		grant_type: 'authorization_code',
+		redirect_uri: process.env.REDIRECT_URI,
+		code,
+		scope: SCOPES.join('%20'),
+	};
+
+	await fetch(auths.token, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: new URLSearchParams({
+			client_id: data.client_id,
+			client_secret: data.client_secret,
+			grant_type: data.grant_type,
+			code: code,
+			redirect_uri: data.redirect_uri,
+			scope: SCOPES.join('%20'),
+		}),
+	})
+		.then((res) => res.json())
+		.then(async (data: ReturnTokens) => {
+			await fetch(`https://discord.com/api/users/@me`, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${data.access_token}`,
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => console.log(data))
+				.catch(console.error);
 		})
 		.catch(console.error);
 }
--- @website/utils/util.ts
+++ @website/utils/util.ts
@@ -4,6 +4,23 @@ interface URLS {
 	revoked?: string;
 }
 
+export interface DataProps {
+	client_id: string;
+	client_secret: string;
+	grant_type: string;
+	redirect_uri: string;
+	code: string | string[];
+	scope: string;
+}
+
+export interface ReturnTokens {
+	access_token: string;
+	expires_in: number;
+	refresh_token: string;
+	scope: string;
+	token_type: string;
+}
+
 export const auths: URLS = {
 	base: 'https://discord.com/api/oauth2/authorize',
 	token: 'https://discord.com/api/oauth2/token',
