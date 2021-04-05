1cd0060b4f1c97d17b7e4ffaf60c805198a6e1fb
Pravesh Kunwar <praveshk04@outlook.com>
2021-04-04 22:14:10 -0400

    (fix): Folder struct.

---
+++ cli/main.ts
@@ -0,0 +1,3 @@
+/* Hi, this is a future cli I will be working on for my bot, to show people how to work on the bot, how different commands work, and etc. 
+Check it out :D. If you want to contriubte, check out contributing.md
+*/
---
+++ electron/main.ts
@@ -0,0 +1,4 @@
+/* Hi, this is a future project I will be working on. This electron folder (name might change later) will be used to create an 
+electron - react - typescript app that will allow users to create discord bots with cool commands and stuff (idk). If you want to contribute
+check out contributing.md
+*/
