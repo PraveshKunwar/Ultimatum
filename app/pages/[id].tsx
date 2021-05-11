86e108ee52cf98e2723f2312b3b9cdf3e3827946
Pravesh Kunwar <praveshisawesome2017@gmail.com>
2021-05-11 12:35:06 -0400

    chore: continue working on auth

---
+++ app/pages/[id].tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const User: NextPage = () => {
+	return <div className="user">HI HIHIHIHIHI</div>;
+};
+
+export default User;
--- app/pages/api/authorized.ts
+++ app/pages/api/authorized.ts
@@ -1,8 +1,38 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import { scopes, URLS } from '../../glob.types';
-
+import nookies from 'nookies';
+import axios from 'axios';
 require('dotenv').config();
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const code = req.query.code as string;
+	const SCOPES = ['identify', 'email', 'connections'];
+	const data = {
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		grant_type: 'authorization_code',
+		redirect_uri: encodeURIComponent('http://localhost:3000/'),
+		code,
+		scope: SCOPES.join('%20'),
+	};
+
+	await fetch(URLS.TOKEN, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: new URLSearchParams({
+			client_id: data.client_id,
+			client_secret: data.client_secret,
+			grant_type: data.grant_type,
+			code: code,
+			redirect_uri: data.redirect_uri,
+			scope: 'identify',
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			res.send(data);
+		})
+		.catch(console.error);
 };
